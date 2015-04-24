32372e7fabce5c2953302106351bbf494c2416a3
spova <povaliaev@gmail.com>
2015-04-25 02:11:13 +0300

    First usable version
    Added max. waterings per night. Trying to make code more structured lol.

--- watering.ino
+++ watering.ino
@@ -1,111 +1,154 @@
 //Digital pins
 int photoResistorPowerSource = 2;
 int soilHumidityPowerSource = 3;
+//L298N relevant
+int l298NPowerSource = 6;
+int motorValveIN1 = 7;
+int motorValveIN2 = 8;
 //Analog pins
 int photoResistor = 0;
 int soilHumidity = 1;
 
-int nightBrightnessLevel = 800;
-int soilHumidityLowLevel = 700;
+int nightLightLevel = 700;
+int soilHumidityLowLevel = 800;
 unsigned long measurementInterval = 2000;  //Overall measurement interval
 unsigned long capacitanceDelay = 500;  //Delay after sourcing power to sensors prior to measurement
-unsigned long interSensorMeasurementInterval = 500;  //Interval between measurements of each sensor
+unsigned long sensorsMeasurementInterval = 500;  //Interval between measurements of each sensor
 int brightnessSeriesAssuringNight = 3;  //Number of contiguous measurements of brightness below threshold assuring night
-int soilHumiditySeriesAssuringValveOpening = 3;  //Number of contiguous measurements of soil humidity below threshold assuring valve opening
+int humiditySeriesAssuringSoilIsDry = 3;  //Number of contiguous measurements of soil humidity below threshold assuring valve opening
 unsigned long wateringDuration = 10000;
 unsigned long wateringHoldTime = 20000;  //Minimum time interval between waterings
-unsigned long valveMotorRunningDuration = 4000;
+unsigned long motorValveRunningDuration = 4000;
+int maxWateringsPerNight = 3;
 
 int measuredSoilHumidityLevel = 0;
-int measuredBrightnessLevel = 0;
+int measuredLightLevel = 0;
 unsigned long lastMeasurementStartTime = 0;
 unsigned long lastWateringStartTime = 0;
-int matchingBrightnessSeries = 0;
+int measurementStage = 0;
+int matchingLightSeries = 0;
 int matchingSoilHumiditySeries = 0;
-boolean valveOpened = false;
-boolean valveMotorIsRunning = true;
-unsigned long valveMotorStartTime = 0;
+int wateringsLastNight = 0;
+boolean valveOpened = true;
+boolean motorValveIsRunning = false;
+unsigned long motorValveStartTime = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(photoResistorPowerSource, OUTPUT);
-  digitalWrite(photoResistorPowerSource, LOW);
   pinMode(soilHumidityPowerSource, OUTPUT);
+  pinMode(l298NPowerSource, OUTPUT);
+  pinMode(motorValveIN1, OUTPUT);
+  pinMode(motorValveIN2, OUTPUT);
   digitalWrite(soilHumidityPowerSource, LOW);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
+  digitalWrite(photoResistorPowerSource, LOW);
 
-  digitalWrite(7, LOW);
-  digitalWrite(8, HIGH);
-  delay(valveMotorRunningDuration);
-
+  // Close valve to be sure
+/*  digitalWrite(l298NPowerSource, HIGH);
+  digitalWrite(motorValveIN1, LOW);
+  digitalWrite(motorValveIN2, HIGH);
+*/
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  if (valveMotorIsRunning && ((millis() - valveMotorStartTime) >= valveMotorRunningDuration)) {
-    digitalWrite(7, LOW);
-    digitalWrite(8, LOW);
-    valveMotorIsRunning = false;
+  // Stop motor valve if it's running enough
+  if (motorValveIsRunning && ((millis() - motorValveStartTime) >= motorValveRunningDuration)) {
+    Serial.println("\tSwitching off motor valve");
+    digitalWrite(l298NPowerSource, LOW);
+    digitalWrite(motorValveIN1, LOW);
+    digitalWrite(motorValveIN2, LOW);
+    motorValveIsRunning = false;
   }
 
-  if ((millis() - lastMeasurementStartTime) >= measurementInterval) {
+  // Start powering sensors
+  if ((measurementStage == 0) && (millis() - lastMeasurementStartTime) >= measurementInterval) {
     lastMeasurementStartTime = millis();
+    measurementStage++;
     digitalWrite(photoResistorPowerSource, HIGH);
     digitalWrite(soilHumidityPowerSource, HIGH);
-    delay(capacitanceDelay);
+  }
 
-    measuredBrightnessLevel = analogRead(photoResistor);
-    Serial.print("Light: ");
-    Serial.print(measuredBrightnessLevel);
-    if (measuredBrightnessLevel < nightBrightnessLevel) {
-      matchingBrightnessSeries++;
+  // Wait little more time for sensors's capacitance to be set and start measuring light level
+  if ((measurementStage == 1) && ((millis() - lastMeasurementStartTime) >= capacitanceDelay)) {
+    measuredLightLevel = analogRead(photoResistor);
+
+    // Check if night is really coming by counting repetitive results below threshold
+    if (measuredLightLevel < nightLightLevel) {
+      matchingLightSeries++;
     }
     else {
-      matchingBrightnessSeries = 0;
-    }
-    Serial.print("\t");
-    Serial.print(matchingBrightnessSeries);
-    delay(interSensorMeasurementInterval);
+      matchingLightSeries = 0;
 
+      // Reset last night waterings counter
+      wateringsLastNight = 0;
+    }
+
+    measurementStage++;
+  }
+
+  // Start measuring soil humidity level
+  if ((measurementStage == 2) && ((millis() - lastMeasurementStartTime) >= (capacitanceDelay + sensorsMeasurementInterval))) {
     measuredSoilHumidityLevel = analogRead(soilHumidity);
-    Serial.print("\tSoil humidity: ");
-    Serial.print(measuredSoilHumidityLevel);
-    Serial.print("\n");
+
+    // Check if soil is really dry by counting repetitive results below threshold
     if (measuredSoilHumidityLevel < soilHumidityLowLevel) {
       matchingSoilHumiditySeries++;
     }
     else {
       matchingSoilHumiditySeries = 0;
     }
+    
+    measurementStage++;
+    
+    // Print some useful info
+    Serial.print("Light: ");
+    Serial.print(measuredLightLevel);
+    Serial.print("\tNightfall? ");
+    if (matchingLightSeries >= brightnessSeriesAssuringNight) {
+      Serial.print("Yes. ");
+      Serial.print(wateringsLastNight);
+      Serial.println(" waterings this night");
+    }
+    else Serial.println("No");
+    Serial.print("Soil humidity: ");
+    Serial.println(measuredSoilHumidityLevel);
+  }
 
+  // Power off sensors
+  if (measurementStage == 3) {
     digitalWrite(photoResistorPowerSource, LOW);
     digitalWrite(soilHumidityPowerSource, LOW);
+    measurementStage = 0;
   }
 
-//  if ((matchingBrightnessSeries == brightnessSeriesAssuringNight) && (matchingSoilHumiditySeries == soilHumiditySeriesAssuringValveOpening) && \
-//      ((millis() - lastWateringStartTime) >= (wateringDuration + wateringHoldTime))) {
-  if ((matchingBrightnessSeries == brightnessSeriesAssuringNight) && \
-      ((millis() - lastWateringStartTime) >= (wateringDuration + wateringHoldTime))) {        
+  // Start watering
+  if (((millis() - lastWateringStartTime) >= (wateringDuration + wateringHoldTime)) && \
+      (motorValveIsRunning == false) && \
+      (wateringsLastNight <= (maxWateringsPerNight - 1)) && \
+      (matchingLightSeries >= brightnessSeriesAssuringNight) \
+      /*&& (matchingSoilHumiditySeries >= humiditySeriesAssuringSoilIsDry)*/) {
     lastWateringStartTime = millis();
-    digitalWrite(7, HIGH);
-    digitalWrite(8, LOW);
-    valveMotorStartTime = millis();
+    digitalWrite(l298NPowerSource, HIGH);
+    digitalWrite(motorValveIN1, HIGH);
+    digitalWrite(motorValveIN2, LOW);
+    motorValveStartTime = millis();
     valveOpened = true;
-    valveMotorIsRunning = true;
+    motorValveIsRunning = true;
+    Serial.println("\tStart watering. Switching on motor valve");
   }
 
+  // Stop watering
   if (valveOpened && ((millis() - lastWateringStartTime) >= wateringDuration)) {
-    digitalWrite(7, LOW);
-    digitalWrite(8, HIGH);
-    valveMotorStartTime = millis();
+    digitalWrite(l298NPowerSource, HIGH);
+    digitalWrite(motorValveIN1, LOW);
+    digitalWrite(motorValveIN2, HIGH);
+    wateringsLastNight++;
+    motorValveStartTime = millis();
     valveOpened = false;
-    valveMotorIsRunning = true;
+    motorValveIsRunning = true;
+    Serial.println("\tStop watering. Switching on motor valve");
   }
-
-
-
+  
 }
